934d28abc803b659639e74576340e3f6bc776d2a
cheesepizza453 <earlykim1209@gmail.com>
2023-05-09 14:51:53 +0900

    :lipstick:Style: 게시글 페이지 반응형 UI 구현

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,23 +1,31 @@
 import { ReactComponent as View } from '../images/view.svg';
 import { ReactComponent as PerpettOn } from '../images/perpett-on.svg';
 import { ReactComponent as PerpettOff } from '../images/perpett-off.svg';
+import { ReactComponent as More } from '../images/more.svg';
+import Comment from '../components/Comment';
+import Dog from '../images/dog.jpeg';
 import Button from '../components/Button/Button';
+import Input from '../components/Input/Input';
 
 function Post() {
   return (
     <div className="mb-64 mt-55 flex flex-col items-center onlyMobile:mt-0">
-      <div className="w-full max-w-[1440px] px-189 onlyMobile:max-w-full onlyMobile:px-0">
+      <div className="w-full max-w-[1162px] px-50  onlyMobile:px-20 onlyMobile:py-30">
         <div className="border-b border-solid border-black-070 pb-32">
-          <p className="text-14 text-black-350">
+          <p className="text-14 text-black-350 onlyMobile:text-12">
             왈독 애견유치원 왈독 - 커뮤니티
           </p>
-          <div className="flex items-center onlyMobile:my-40">
-            <div className="w-full onlyMobile:pl-15">
-              <p className="text-28 font-bold ">커뮤니티 게시글꺼</p>
+          <div className="flex items-center">
+            <div className="w-full">
+              <p className="text-28 font-bold onlyMobile:text-22">
+                커뮤니티 게시글꺼
+              </p>
               <div className="mt-12 flex flex w-full items-center justify-between">
                 <div className="flex flex-col">
                   <div className="mb-12 flex items-center">
-                    <div className="list-user-image-small" />
+                    <div className="user-profile h-24 w-24">
+                      <img src={Dog} alt="임시이미지" />
+                    </div>
                     <p className="relative pl-5 pr-12 text-14 text-black-900 onlyMobile:text-12">
                       콩이쫑이맘
                     </p>
@@ -41,7 +49,7 @@ function Post() {
           </div>
         </div>
         <div className="border-b border-solid border-black-070 pb-24">
-          <div className="py-32">
+          <div className="py-32 onlyMobile:py-24 onlyMobile:text-14">
             포스트 컨텐츠 영역포스트 컨텐츠 영역포스트 컨텐츠 영역포스트 컨텐츠
             영역포스트 컨텐츠 영역포스트 컨텐츠 영역포스트 컨텐츠 영역포스트
             컨텐츠 영역포스트 컨텐츠 영역포스트 컨텐츠 영역포스트 컨텐츠
@@ -53,34 +61,48 @@ function Post() {
             컨텐츠 영역포스트 컨텐츠 영역
           </div>
           <div className="mb-40">
-            <button className="mr-15 text-14 text-black-350" type="button">
+            <button
+              className="mr-15 text-14 text-black-350 onlyMobile:text-12"
+              type="button"
+            >
               수정
             </button>
-            <button className="text-14 text-black-350" type="button">
+            <button
+              className="text-14 text-black-350 onlyMobile:text-12"
+              type="button"
+            >
               삭제
             </button>
           </div>
           <div className="flex justify-between">
-            <div className="flex text-16">
+            <div className="items:center flex text-16 onlyMobile:text-14">
               <PerpettOff width="24" height="24" className="mr-10" />
               좋아요
               <span className="pl-5 font-bold">21</span>
             </div>
             <div className="flex">
-              <Button className="btn-size-m border-gray mr-10">이전글</Button>
+              <Button className="btn-size-m border-gray mr-10 ">이전글</Button>
               <Button className="btn-size-m border-gray">다음글</Button>
             </div>
           </div>
         </div>
         <div className="py-30">
-          <p className="mb-30 text-16 font-bold">
+          <p className="mb-30 text-16 font-bold onlyMobile:text-14">
             댓글<span className="pl-2">3</span>
           </p>
-          <div className="w-full rounded-[12px] bg-black-025 p-30">
-            {/* 댓글 컴포넌트화 */}
-            <div>올리맘</div>
+          <div className="w-full rounded-[12px] bg-black-025 px-32 pt-32">
+            <Comment />
+            <Comment />
           </div>
-          <div>댓글 등록 인풋</div>
+          <div className="mt-20 flex w-full">
+            <Input
+              placeholder="댓글을 입력해주세요."
+              className="!margin-0 w-full"
+            />
+            <Button className="btn-size-l color-yellow ml-8 shrink-0 ">
+              댓글 등록
+            </Button>
+          </div>
         </div>
       </div>
     </div>
