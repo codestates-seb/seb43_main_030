dc34c39936b0cfa5a187ac6a27122c5fcace7517
moonozz <http0127@naver.com>
2023-05-09 16:29:50 +0900

    :lipstick:Style: Login ui 완성

--- src/App.js
+++ src/App.js
@@ -41,7 +41,19 @@ function App() {
 
   const hideFooterRoutes = ['/map', '/login', '/signup', '/find-password'];
   const shouldHideFooter = hideFooterRoutes.includes(location.pathname);
+
   return (
+    <div className="h-[calc(100vh-80px)]">
+      {isMobile ? <MHeader /> : <PcHeader />}
+
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/map" element={<Map />} />
+        <Route path="/community" element={<Community />} />
+      </Routes>
+      {shouldHideFooter ? null : <Footer />}
+    </div>
     // <div className="App">
     //   <Button className="btn-size-s color-yellow ">버튼</Button>
     //   <Button className="btn-size-m color-yellow">버튼</Button>
@@ -116,17 +128,6 @@ function App() {
     //     bgColor="bg-red-400"
     //   />
     // </div>
-    <div className="h-[calc(100vh-80px)]">
-      {isMobile ? <MHeader /> : <PcHeader />}
-
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/map" element={<Map />} />
-        <Route path="/community" element={<Community />} />
-      </Routes>
-      {shouldHideFooter ? null : <Footer />}
-    </div>
   );
 }
 
