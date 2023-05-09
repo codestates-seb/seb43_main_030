81f0e509c985bddf62832a248d7da31b7baf59ac
jieuny0314 <ccomo0313@naver.com>
2023-05-09 10:17:11 +0900

    :zap: Feat: 기본 마커에 유치원 이름 표시

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -2,18 +2,15 @@ import {
   GoogleMap,
   useJsApiLoader,
   MarkerF,
-  InfoWindow,
+  InfoWindowF,
 } from '@react-google-maps/api';
 import { useState, useCallback, memo, useEffect } from 'react';
+import { ReactComponent as MarkerIcon } from '../images/logo-symbol.svg';
+import Pin from '../components/Pin';
 
 const containerStyle = {
   width: '100%',
   height: '100%',
-};
-
-const center = {
-  lat: 37.5303564,
-  lng: 126.9897517,
 };
 
 const myStyles = [
@@ -31,27 +28,44 @@ function Map() {
   });
 
   const markerValues = [
-    { lat: 37.536631, lng: 126.977376 },
-    { lat: 37.5195, lng: 127.0239 },
-    { lat: 37.495919, lng: 126.959497 },
+    { id: 1, lat: 37.536631, lng: 126.977376, name: '왈왈유치원' },
+    { id: 2, lat: 37.5195, lng: 127.0239, name: '애견유치원' },
+    { id: 3, lat: 37.495919, lng: 126.959497, name: '멍더가든' },
   ];
 
   const [map, setMap] = useState(null);
 
-  const onLoad = useCallback(function callback(mapValue) {
-    const centerLatLng = new window.google.maps.LatLng(center.lat, center.lng);
-    const bounds = new window.google.maps.LatLngBounds(centerLatLng);
-    const circle = new window.google.maps.Circle({
-      center: centerLatLng,
-      radius: 5000, // 500m 반경
-    });
-    bounds.union(circle.getBounds());
-    mapValue.fitBounds(bounds);
-  }, []);
+  const [center, setCenter] = useState({
+    lat: 37.5303564,
+    lng: 126.9897517,
+  });
+
+  const onLoad = useCallback(
+    function callback(mapValue) {
+      const centerLatLng = new window.google.maps.LatLng(
+        center.lat,
+        center.lng,
+      );
+      const bounds = new window.google.maps.LatLngBounds(centerLatLng);
+      const circle = new window.google.maps.Circle({
+        center: centerLatLng,
+        radius: 5000, // 500m 반경
+      });
+      bounds.union(circle.getBounds());
+      mapValue.fitBounds(bounds);
+    },
+    [center.lat, center.lng],
+  );
 
   const onUnmount = useCallback(function callback(mapValue) {
     setMap(null);
   }, []);
+
+  const [clickedMarker, setClickedMarker] = useState(null);
+
+  const handleMarkerClick = markerValue => {
+    setClickedMarker(markerValue);
+  };
 
   return isLoaded ? (
     <GoogleMap
@@ -64,7 +78,20 @@ function Map() {
     >
       {markerValues.map(markerValue => {
         return (
-          <MarkerF position={{ lat: markerValue.lat, lng: markerValue.lng }} />
+          <MarkerF
+            key={markerValue.id}
+            position={{ lat: markerValue.lat, lng: markerValue.lng }}
+            onClick={() => handleMarkerClick(markerValue)}
+          >
+            {clickedMarker && clickedMarker.id === markerValue.id && (
+              <InfoWindowF
+                position={{ lat: clickedMarker.lat, lng: clickedMarker.lng }}
+                onCloseClick={() => setClickedMarker(null)}
+              >
+                <div>{clickedMarker.name}</div>
+              </InfoWindowF>
+            )}
+          </MarkerF>
         );
       })}
     </GoogleMap>
