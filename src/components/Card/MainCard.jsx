b7f2dcd5d81f5ddc1e9ce28704347dd631e6ebe8
jieuny0314 <ccomo0313@naver.com>
2023-05-10 15:26:09 +0900

    :lipstick: Style : comment update input css 수정"

--- src/components/Card/MainCard.jsx
+++ src/components/Card/MainCard.jsx
@@ -6,8 +6,12 @@ function MainCard(props) {
   const { name, ratedReviewsAvg, ratedReviewsCount, locations, id } = props;
   return (
     <Link to={`/kindergarten/${id}`}>
-      <div className="mb-40 min-w-302">
-        <img className="mb-20 h-248 w-full rounded-2xl" src={Dog} alt="dog" />
+      <div className="mb-40 min-w-302 hover:mt-[-2px]">
+        <img
+          className="mb-20 h-248 w-full rounded-2xl hover:shadow-cardShadow"
+          src={Dog}
+          alt="dog"
+        />
         <div className="justifty-center flex w-full flex-col items-start px-8 py-0">
           <div className="text-18 font-bold">{name}</div>
           <div className="flex-center mb-8 text-14">
