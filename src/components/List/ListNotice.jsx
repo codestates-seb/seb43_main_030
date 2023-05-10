26c2a000bd844e3384b31e4b97151d7f5bb7e03e
moonozz <http0127@naver.com>
2023-05-10 13:31:08 +0900

    lipstick:Style: 화면 줄었을 때 text 잘려보이게

--- src/components/List/ListNotice.jsx
+++ src/components/List/ListNotice.jsx
@@ -2,17 +2,17 @@ function ListNotice() {
   return (
     <div>
       <ul>
-        <li className="list-flex max-w-664">
-          <div className="mr-24 flex w-504 flex-col justify-start">
-            <p className="list-title mb-8 ">
+        <li className="list-flex w-full">
+          <div className="flex w-full flex-col justify-start">
+            <p className="list-title h-text-max mb-8 max-h-[30px]">
               공지제목공지제목공지제목공지제목공지제목공지제목
             </p>
-            <p className="list-content">
+            <p className="list-content h-text-max max-h-[50px]">
               공지내용미리보기공지내용미리보기공지내용미리보기공지내용미리보기공지내용미리보기공지내용미리보기
             </p>
             <span className="list-gray-small mt-16">2023년 00월 00일</span>
           </div>
-          <div className="list-notice-image" />
+          <div className="list-notice-image ml-24" />
         </li>
       </ul>
     </div>
--- src/components/List/ListReview.jsx
+++ src/components/List/ListReview.jsx
@@ -4,20 +4,25 @@ function ListReview() {
   return (
     <div>
       <ul>
-        <li className="list-flex max-w-664">
+        <li className="list-flex w-full">
           <div className="mr-24 w-full">
             <div className="flex w-full items-center justify-between">
-              <div className="flex">
+              <div className="flex w-full">
                 <div className="list-user-image mr-16" />
-                <div>
-                  <p className="list-title">작성자</p>
-                  <RatingStar />
+                <div className="flex w-full flex-col">
+                  <div className="flex items-center gap-2">
+                    <p className="list-title">작성자</p>
+                    <p className="text-14 text-black-350">이메일</p>
+                  </div>
+                  <div className="flex w-full items-center justify-between">
+                    <RatingStar />
+                    <p className="list-gray-small">23.01.01</p>
+                  </div>
                 </div>
               </div>
-              <p className="list-gray-small">2023년01월01일</p>
             </div>
-            <p className="list-content mt-16">
-              후기내용후기내용후기내용후기내용후기내용미리보기
+            <p className="list-content h-text-max mt-16 max-h-[50px]">
+              후기내용후기내용후기내용후기내용후기내용미리보기용후기내용미리보기
             </p>
             <p className="py-4 text-left text-14 font-bold text-black-900">
               더보기
