263e9ac503d99df1fed68bccf397a3ca22d111fa
jieuny0314 <ccomo0313@naver.com>
2023-05-10 14:10:49 +0900

    :zap: Feat : 메인에서 유치원 카드 누르면 각 커뮤니티로 Route 추가

--- src/components/Card/MainCard.jsx
+++ src/components/Card/MainCard.jsx
@@ -1,22 +1,25 @@
+import { Link } from 'react-router-dom';
 import Dog from '../../images/dog.jpeg';
 import { ReactComponent as StarOn } from '../../images/star-on.svg';
 
 function MainCard(props) {
-  const { name, ratedReviewsAvg, ratedReviewsCount, locations } = props;
+  const { name, ratedReviewsAvg, ratedReviewsCount, locations, id } = props;
   return (
-    <div className="mb-40 min-w-302">
-      <img className="mb-20 h-248 w-full rounded-2xl" src={Dog} alt="dog" />
-      <div className="justifty-center flex w-full flex-col items-start px-8 py-0">
-        <div className="text-18 font-bold">{name}</div>
-        <div className="flex-center mb-8 text-14">
-          <StarOn className="mr-4 inline-block" />
-          <span className="mr-4">{ratedReviewsAvg}</span>({ratedReviewsCount})
-        </div>
-        <div className="w-full overflow-hidden text-ellipsis whitespace-nowrap text-14">
-          {locations}
+    <Link to={`/kindergarten/${id}`}>
+      <div className="mb-40 min-w-302">
+        <img className="mb-20 h-248 w-full rounded-2xl" src={Dog} alt="dog" />
+        <div className="justifty-center flex w-full flex-col items-start px-8 py-0">
+          <div className="text-18 font-bold">{name}</div>
+          <div className="flex-center mb-8 text-14">
+            <StarOn className="mr-4 inline-block" />
+            <span className="mr-4">{ratedReviewsAvg}</span>({ratedReviewsCount})
+          </div>
+          <div className="w-full overflow-hidden text-ellipsis whitespace-nowrap text-14">
+            {locations}
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
