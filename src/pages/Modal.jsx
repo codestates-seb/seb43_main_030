3e5ea14c04af9bf3d11de36cb88e540f83a93ba5
cheesepizza453 <earlykim1209@gmail.com>
2023-05-11 16:24:05 +0900

    :lipstick:Style: 후기 작성 모바일 반응형 구현

--- src/pages/Modal.jsx
+++ src/pages/Modal.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react';
-import { Link, Navigate, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 import Dog from '../images/dog.jpeg';
@@ -7,13 +7,13 @@ import RatingStar from '../components/RatingStar';
 import TextArea from '../components/TextArea';
 import UploadImage from '../components/UploadImage';
 import Button from '../components/Button/Button';
-import { ReactComponent as ArrowNext } from '../images/arrow-next.svg';
-import { ReactComponent as ArrowPrev } from '../images/arrow-prev.svg';
 import { ReactComponent as Star } from '../images/star-on.svg';
+import { ReactComponent as Close } from '../images/close.svg';
 
-function Modal() {
+function Modal(props) {
+  const { onClick } = props;
+
   const [text, setText] = useState('');
-  const navigate = useNavigate();
 
   const textCount = event => {
     setText(event.target.value);
@@ -21,56 +21,64 @@ function Modal() {
 
   return (
     <>
-      <div className="flex-center flex">
-        <div className="z-[101] my-80 h-[calc(100vh-160px)] w-608 rounded-[16px] bg-white">
-          <div className="relative h-full p-30">
-            {/* 후기 작성하기 */}
-            <div className="relative flex justify-center">
-              <p className="write-title">후기 작성하기</p>
-              <button type="button" className="absolute right-0 top-[-4px]">
-                X
-              </button>
-            </div>
-
-            {/* 유치원 정보 */}
-            <div className="mt-25 flex border-b-[1px] border-black-070 pb-25">
-              <div className="user-profile mr-15 h-116 w-116">
-                <img src={Dog} alt="임시 이미지" />
+      <div className="flex justify-center">
+        <div className="fixed top-[10px] z-[101] my-80 h-[calc(100vh-160px)] w-608 rounded-[16px] bg-white onlyMobile:top-0 onlyMobile:my-0 onlyMobile:h-full onlyMobile:w-full onlyMobile:rounded-0">
+          <div className="relative flex h-full flex-col p-30">
+            <div className=" w-full shrink-0">
+              {/* 후기 작성하기 */}
+              <div className="relative flex justify-center">
+                <p className="write-title">후기 작성하기</p>
+                <button
+                  type="button"
+                  className="absolute right-0 top-[-4px]"
+                  onClick={onClick}
+                >
+                  <Close />
+                </button>
               </div>
-              <div className="flex flex-col justify-center">
-                <p className="write-title">놀펫 강아지 어쩌구 유치원</p>
-                <p className="flex items-center text-14">
-                  <Star /> 4.3 (12)
-                </p>
-                <p className="mt-6 text-14">주소주소 성수동 어쩌구</p>
+
+              {/* 유치원 정보 */}
+              <div className="mt-25 flex border-b-[1px] border-black-070 pb-25">
+                <div className="user-profile mr-15 h-116 w-116 onlyMobile:h-96 onlyMobile:w-96">
+                  <img src={Dog} alt="임시 이미지" />
+                </div>
+                <div className="flex flex-col justify-center">
+                  <p className="write-title">놀펫 강아지 어쩌구 유치원</p>
+                  <p className="flex items-center text-14">
+                    <Star /> 4.3 (12)
+                  </p>
+                  <p className="mt-6 text-14">주소주소 성수동 어쩌구</p>
+                </div>
               </div>
             </div>
             {/* 별점 */}
-            <div className="mt-25 flex items-center border-b-[1px] border-black-070 pb-25">
-              <p className="write-title mr-15">유치원은 어떠셨나요?</p>
-              <RatingStar />
-            </div>
-            {/* 후기 */}
-            <div className="mt-25 flex flex-col border-b-[1px] border-black-070 pb-25">
-              <p className="write-title mb-15 mr-15">
-                솔직한 후기를 남겨주세요.
-              </p>
-              <TextArea
-                areaClass="h-135 py-20 "
-                value={text}
-                onChange={textCount}
-                maxLength="200"
-                placeholder="후기를 남겨주세요."
-              />
-            </div>
-            {/* 사진등록 */}
-            <div className="mt-25 flex flex-col  pb-25">
-              <p className="write-title mb-15 mr-15">사진을 등록해주세요.</p>
-              <div className="flex">
-                <UploadImage />
-                <UploadImage />
-                <UploadImage />
-                <UploadImage />
+            <div className="mb-30 overflow-y-scroll">
+              <div className="mt-25 flex items-center border-b-[1px] border-black-070 pb-25">
+                <p className="write-title mr-15">유치원은 어떠셨나요?</p>
+                <RatingStar />
+              </div>
+              {/* 후기 */}
+              <div className="mt-25 flex flex-col border-b-[1px] border-black-070 pb-25">
+                <p className="write-title mb-15 mr-15">
+                  솔직한 후기를 남겨주세요.
+                </p>
+                <TextArea
+                  areaClass="h-135 py-20 "
+                  value={text}
+                  onChange={textCount}
+                  maxLength="200"
+                  placeholder="후기를 남겨주세요."
+                />
+              </div>
+              {/* 사진등록 */}
+              <div className="mt-25 flex flex-col  pb-25">
+                <p className="write-title mb-15 mr-15">사진을 등록해주세요.</p>
+                <div className="flex">
+                  <UploadImage />
+                  <UploadImage />
+                  <UploadImage />
+                  <UploadImage />
+                </div>
               </div>
             </div>
             {/* 버튼 */}
@@ -80,7 +88,7 @@ function Modal() {
           </div>
         </div>
       </div>
-      <div className="fixed left-0 top-0 z-[100] h-[100vh] w-[100vw] bg-[rgba(0,0,0,0.7)]" />
+      <div className="fixed left-0 top-0 z-[100] h-[100vh] w-[100vw] bg-[rgba(0,0,0,0.7)] onlyMobile:hidden" />
     </>
   );
 }
