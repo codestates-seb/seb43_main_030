199bdb24e4e602e4511bce5c60db83feb1e60f6f
jieuny0314 <ccomo0313@naver.com>
2023-05-12 13:55:29 +0900

    :lipstick: Style : css 간격 수정

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -5,10 +5,10 @@ function NotFound() {
     <div className="flex-center relative flex-col">
       <div className="flex-center mt-80 h-640 w-[100%] max-w-[1440px] flex-col px-80">
         <img src={NoList} alt="NoList" className="h-160 w-160" />
-        <span className="h-78 text-56 font-black text-yellow-500">404</span>
-        <span className="text-18 text-black-350">
-          여기는 페이지가 없어요...
-        </span>
+        <div className="flex-center h-70 text-56 font-black text-yellow-500">
+          404
+        </div>
+        <div className="text-18 text-black-350">여기는 페이지가 없어요...</div>
       </div>
     </div>
   );
