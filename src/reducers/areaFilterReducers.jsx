b2c522fe9b3ec3075a86f005c7c10daca3560523
jieuny0314 <ccomo0313@naver.com>
2023-05-16 13:23:04 +0900

    :zap: Feat : Redux-persist 추가

--- src/reducers/areaFilterReducers.jsx
+++ src/reducers/areaFilterReducers.jsx
@@ -1,4 +1,6 @@
 import { createReducer, combineReducers } from '@reduxjs/toolkit';
+import { persistReducer } from 'redux-persist';
+import storageSession from 'redux-persist/lib/storage/session';
 import {
   setAreaFilter,
   setCenter,
@@ -78,4 +80,23 @@ const rootReducer = combineReducers({
   auth: authReducer,
 });
 
-export default rootReducer;
+const persistConfig = {
+  key: 'root',
+  storage: storageSession, // 사용할 스토리지를 정의해요.
+  whitelist: [
+    'curUser',
+    'user',
+    'curProfile',
+    'kinderGartens',
+    'areaFilter',
+    'cetner',
+    'inputValue',
+    'searchValue',
+    'auth',
+  ], // 유지 할 데이터를 정의해요
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+// reducer와 위에서 설정 한 persist설정을 합쳐요
+
+export default persistedReducer;
