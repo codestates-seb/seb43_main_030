d4663cfc8a83cf7ecabaf9e752818b120de8669b
moonozz <http0127@naver.com>
2023-05-22 00:59:09 +0900

    :lipstick:Style: 리뷰 모달 ui 수정

--- src/components/List/ConfirmReview.jsx
+++ src/components/List/ConfirmReview.jsx
@@ -75,137 +75,139 @@ function ConfirmReview(props) {
   }, [kinderInfo.image, kinderInfo.reviewImageUrl]);
 
   return (
-    <div className="flex w-full max-w-608 justify-center py-80">
-      <div className="fixed top-[10px] z-[101] my-80 h-[calc(100vh-160px)] max-w-608 rounded-[16px] bg-white onlyMobile:top-0 onlyMobile:my-0 onlyMobile:h-full onlyMobile:w-full onlyMobile:rounded-0">
-        <div className="relative flex h-full w-[100%] flex-col p-30">
-          <div className=" w-full shrink-0">
-            <div className="relative flex justify-center">
-              <button
-                type="button"
-                className="absolute right-0 top-[-4px]"
-                onClick={() => offReviewModal()}
-              >
-                <Close />
-              </button>
-            </div>
-            <div className="mb-24 mt-30 flex w-600">
-              <div className="flex w-full items-center justify-between">
-                <div className="flex w-full">
-                  <div className="list-user-image mr-16">
-                    <img
-                      src={kinderInfo.profileImageUrl}
-                      alt="profileImg"
-                      className="rounded-xl"
-                    />
-                  </div>
-                  <div className="flex w-full flex-col">
-                    <div className="flex items-center gap-2">
-                      <p className="text-16 font-bold onlyMobile:text-14">
-                        {kinderInfo.profileName}
-                      </p>
-                      <p className="text-14 text-black-350 onlyMobile:text-12">
-                        {kinderInfo.email}
-                      </p>
+    <>
+      <div className="flex justify-center">
+        <div className="fixed top-[10px] z-40 my-80 h-[calc(100vh-160px)] max-w-608 rounded-[16px] bg-white onlyMobile:top-0 onlyMobile:my-0 onlyMobile:h-full onlyMobile:w-full onlyMobile:rounded-0">
+          <div className="relative flex h-full w-[100%] flex-col p-30">
+            <div>
+              <div className="relative flex justify-center">
+                <button
+                  type="button"
+                  className="absolute right-0 top-[-4px]"
+                  onClick={() => offReviewModal()}
+                >
+                  <Close />
+                </button>
+              </div>
+              <div className="mb-24 mt-30 flex w-600">
+                <div className="flex w-full items-center justify-between">
+                  <div className="flex w-full">
+                    <div className="list-user-image mr-16">
+                      <img
+                        src={kinderInfo.profileImageUrl}
+                        alt="profileImg"
+                        className="rounded-xl"
+                      />
                     </div>
-                    <div className="flex w-full items-center justify-between">
-                      <div className="star flex">{starScore()}</div>
-                      <p className="list-gray-small pr-[10%]">
-                        {dateCalculate(kinderInfo.createdAt)}
-                      </p>
+                    <div className="flex w-full flex-col">
+                      <div className="flex items-center gap-2">
+                        <p className="text-16 font-bold onlyMobile:text-14">
+                          {kinderInfo.profileName}
+                        </p>
+                        <p className="text-14 text-black-350 onlyMobile:text-12">
+                          {kinderInfo.email}
+                        </p>
+                      </div>
+                      <div className="flex w-full items-center justify-between">
+                        <div className="star flex">{starScore()}</div>
+                        <p className="list-gray-small pr-[10%]">
+                          {dateCalculate(kinderInfo.createdAt)}
+                        </p>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-              {/* <div className="mr-24 h-[75%] w-full">
-                <p className="list-content h-text-max mt-16 max-h-[50px]">
-                  {kinderInfo.contents}
-                </p>
-                {kinderInfo.imageUrl ? (
-                  <img src={kinderInfo.imageUrl} alt="img" />
-                ) : (
-                  ''
-                )}
-              </div> */}
-            </div>
-            <div className="h-[calc(80vh-160px)]">
-              <div className="flex h-[calc(80vh-160px)] flex-col overflow-auto px-8">
-                <p className="mb-24">{kinderInfo.content}</p>
-
-                {kinderInfo.reviewImageUrl ? (
-                  <div className="w-[100%] flex-col">
-                    <img src={kinderInfo.reviewImageUrl} alt="reviewImg" />
-                  </div>
-                ) : (
-                  <p className="text-black-350">
-                    리뷰에 등록된 사진이 없습니다...🥹
+                {/* <div className="mr-24 h-[75%] w-full">
+                  <p className="list-content h-text-max mt-16 max-h-[50px]">
+                    {kinderInfo.contents}
                   </p>
-                )}
+                  {kinderInfo.imageUrl ? (
+                    <img src={kinderInfo.imageUrl} alt="img" />
+                  ) : (
+                    ''
+                  )}
+                </div> */}
+              </div>
+              <div className="h-[calc(80vh-160px)]">
+                <div className="flex h-[calc(80vh-160px)] flex-col overflow-auto px-8">
+                  <p className="mb-24">{kinderInfo.content}</p>
+
+                  {kinderInfo.reviewImageUrl ? (
+                    <div className="w-[100%] flex-col">
+                      <img src={kinderInfo.reviewImageUrl} alt="reviewImg" />
+                    </div>
+                  ) : (
+                    <p className="text-black-350">
+                      리뷰에 등록된 사진이 없습니다...🥹
+                    </p>
+                  )}
+                </div>
+              </div>
+            </div>
+            <div className="absolute bottom-0 z-10 w-548 bg-white">
+              <div className="mr-10 flex flex-col py-12">
+                <div className="flex justify-end">
+                  <button
+                    className="mr-15 text-14 text-black-350 onlyMobile:text-12"
+                    type="button"
+                    onClick={() => {
+                      // offReviewModal();
+                      onEditModal();
+                    }}
+                  >
+                    수정
+                  </button>
+                  <button
+                    className="text-14 text-black-350 onlyMobile:text-12"
+                    type="button"
+                    onClick={deleteReview}
+                  >
+                    삭제
+                  </button>
+                </div>
+              </div>
+              <div className="mb-25 flex border-t-[1px] border-black-070 pt-25">
+                <div className="user-profile mr-15 h-116 w-116 onlyMobile:h-96 onlyMobile:w-96">
+                  {kindergartenImageUrl ? (
+                    <img src={kindergartenImageUrl} alt="img" />
+                  ) : (
+                    <img src={profile} alt="defaultImage" />
+                  )}
+                </div>
+                <div className="flex flex-col justify-center">
+                  <p className="write-title">
+                    {kindergartenName &&
+                      kindergartenName.slice().replace(/"/g, '')}
+                  </p>
+                  <p className="flex items-center text-14">
+                    <Star />
+                    {kindergartenRatedReviewsAvg?.toFixed(2)}
+                    {kindergartenRatedReviewsCount}
+                  </p>
+                  <p className="mt-6 text-14">
+                    {kindergartenLocations?.slice().replace(/"/g, '')}
+                  </p>
+                </div>
               </div>
             </div>
           </div>
-          <div className="absolute bottom-0 z-10 w-548 bg-white">
-            <div className="mr-10 flex flex-col py-12">
-              <div className="flex justify-end">
-                <button
-                  className="mr-15 text-14 text-black-350 onlyMobile:text-12"
-                  type="button"
-                  onClick={() => {
-                    // offReviewModal();
-                    onEditModal();
-                  }}
-                >
-                  수정
-                </button>
-                <button
-                  className="text-14 text-black-350 onlyMobile:text-12"
-                  type="button"
-                  onClick={deleteReview}
-                >
-                  삭제
-                </button>
-              </div>
-            </div>
-            <div className="mb-25 flex border-t-[1px] border-black-070 pt-25">
-              <div className="user-profile mr-15 h-116 w-116 onlyMobile:h-96 onlyMobile:w-96">
-                {kindergartenImageUrl ? (
-                  <img src={kindergartenImageUrl} alt="img" />
-                ) : (
-                  <img src={profile} alt="defaultImage" />
-                )}
-              </div>
-              <div className="flex flex-col justify-center">
-                <p className="write-title">
-                  {kindergartenName &&
-                    kindergartenName.slice().replace(/"/g, '')}
-                </p>
-                <p className="flex items-center text-14">
-                  <Star />
-                  {kindergartenRatedReviewsAvg?.toFixed(2)}
-                  {kindergartenRatedReviewsCount}
-                </p>
-                <p className="mt-6 text-14">
-                  {kindergartenLocations?.slice().replace(/"/g, '')}
-                </p>
-              </div>
-            </div>
-          </div>
+          {/* {console.log(kinderInfo.ratedReview)} */}
+          {editModal ? (
+            <Modal
+              className="absolute left-0 top-0"
+              title="후기 수정하기"
+              onClick={offEditModal}
+              prevRatedReview={kinderInfo.ratedReview}
+              prevText={kinderInfo.content}
+              prevImage={prevImage}
+              setPrevImage={setPrevImage}
+              reviewId={kinderInfo.reviewId}
+            />
+          ) : null}
         </div>
-        {console.log(kinderInfo.ratedReview)}
-        {editModal ? (
-          <Modal
-            className="absolute left-0 top-0"
-            title="후기 수정하기"
-            onClick={offEditModal}
-            prevRatedReview={kinderInfo.ratedReview}
-            prevText={kinderInfo.content}
-            prevImage={prevImage}
-            setPrevImage={setPrevImage}
-            reviewId={kinderInfo.reviewId}
-          />
-        ) : null}
       </div>
-      <div className="fixed left-0 top-0 z-[100] h-[100vh] w-[100vw] bg-[rgba(0,0,0,0.4)] onlyMobile:hidden" />
-    </div>
+      <div className="fixed left-0 top-0 z-30 h-[100vh] w-[100vw] bg-[rgba(0,0,0,0.4)] onlyMobile:hidden" />
+    </>
   );
 }
 export default ConfirmReview;
