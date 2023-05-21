415e45b99d391352c07fdca39288bdee1f7d9205
mogu616 <jtk6747@gmail.com>
2023-05-21 21:42:02 +0900

    :zap: Feat : cdn 구현

--- src/main/java/com/kids/SEB_main_030/global/image/service/ImageUploader.java
+++ src/main/java/com/kids/SEB_main_030/global/image/service/ImageUploader.java
@@ -32,9 +32,33 @@ public class ImageUploader {
     private String defaultKindergartenImage;
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
+    @Value("${cloud.aws.cloud-front}")
+    private String cloudFrontUrl;
 
 
-    // 다중 이미지 업로드(kindergarten, post)
+    // 다중 이미지 업로드(kindergarten, post) S3
+//    public List<String> upload(List<MultipartFile> multipartFile, String location) {
+//        List<String> imgUrlList = new ArrayList<>();
+//
+//        // forEach 구문을 통해 multipartFile로 넘어온 파일들 하나씩 fileNameList에 추가
+//        for (MultipartFile file : multipartFile) {
+//            String fileName = createFileName(file.getOriginalFilename());
+//            ObjectMetadata objectMetadata = new ObjectMetadata();
+//            objectMetadata.setContentLength(file.getSize());
+//            objectMetadata.setContentType(file.getContentType());
+//
+//            try(InputStream inputStream = file.getInputStream()) {
+//                s3Client.putObject(new PutObjectRequest(bucket + location, fileName, inputStream, objectMetadata)
+//                        .withCannedAcl(CannedAccessControlList.PublicRead));
+//                imgUrlList.add(s3Client.getUrl(bucket + location, fileName).toString());
+//            } catch(IOException e) {
+//                throw new LogicException(CustomException.IMAGE_UPLOAD_ERROR);
+//            }
+//        }
+//        return imgUrlList;
+//    }
+
+    // S3 이미지 업로드
     public List<String> upload(List<MultipartFile> multipartFile, String location) {
         List<String> imgUrlList = new ArrayList<>();
 
@@ -48,7 +72,9 @@ public class ImageUploader {
             try(InputStream inputStream = file.getInputStream()) {
                 s3Client.putObject(new PutObjectRequest(bucket + location, fileName, inputStream, objectMetadata)
                         .withCannedAcl(CannedAccessControlList.PublicRead));
-                imgUrlList.add(s3Client.getUrl(bucket + location, fileName).toString());
+//                imgUrlList.add(s3Client.getUrl(bucket + location, fileName).toString());
+                // cloud-front url 추가
+                imgUrlList.add(cloudFrontUrl + location + "/" + fileName);
             } catch(IOException e) {
                 throw new LogicException(CustomException.IMAGE_UPLOAD_ERROR);
             }
