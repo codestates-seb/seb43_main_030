f60611c4420e8ff6c58290e79da67f237f054f8f
JSeungHyun <83762088+JSeungHyun@users.noreply.github.com>
2023-05-22 11:09:14 +0900

    :zap: Feat : 로그인 실패 시 리디렉션

--- src/main/java/com/kids/SEB_main_030/global/security/oauth2/handler/OAuth2LoginFailureHandler.java
+++ src/main/java/com/kids/SEB_main_030/global/security/oauth2/handler/OAuth2LoginFailureHandler.java
@@ -17,6 +17,7 @@ public class OAuth2LoginFailureHandler implements AuthenticationFailureHandler {
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
         response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
         response.getWriter().write("로그인 실패! 로그를 확인해주세요.");
+        response.sendRedirect("http://testqjzlt.s3-website.ap-northeast-2.amazonaws.com/");
         log.info("소셜 로그인에 실패했습니다. 에러 메시지 : {}", exception.getMessage());
     }
 }
